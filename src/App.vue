a2a100d23b26997cd75248107bcad5b8067b15e2
Nuyoah <13056489+zhou-jinfang@user.noreply.gitee.com>
2023-12-17 05:38:17 +0000

    首次上传
    Signed-off-by: Nuyoah <13056489+zhou-jinfang@user.noreply.gitee.com>

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { onLaunch} from "@dcloudio/uni-app";
+onLaunch(() => {
+ const get_data = uni.getStorageSync("MenuButton")
+  if(!get_data) {
+	  const res = uni.getMenuButtonBoundingClientRect()
+	  uni.setStorageSync("MenuButton",res)
+  }
+});
+</script>
+<style>
+  /* 骨架屏 */
+  .skeleton-view{
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #FFFFFF;
+    z-index: 999;
+    overflow: hidden;
+  }
+  .skeleton-global{
+    background: #f3f3f3;
+    border-radius: 8rpx;
+    animation: blink 1.2s ease-in-out infinite;
+  }
+  @keyframes blink{
+      50%{
+        opacity: 0.5;
+      }
+  }
+  </style>

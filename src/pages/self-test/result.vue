7a7f75de382b270fcc239c211aedc1375365d985
Nuyoah <13056489+zhou-jinfang@user.noreply.gitee.com>
2023-12-17 05:56:22 +0000

    首次上传
    Signed-off-by: Nuyoah <13056489+zhou-jinfang@user.noreply.gitee.com>

---
+++ src/pages/self-test/result.vue
@@ -0,0 +1,167 @@
+<template>
+	<view class="result-title text-black" style="font-size: 36rpx;">结果分析</view>
+	<view class="texts">本测试及结果由AI得出，仅供参考，不能作为诊断及诊疗的依据</view>
+	<view class="result" v-for="(item,index) in test_res" :key='index'>
+		<text class="result-tit-b">{{item.scope}}:{{item.result}}</text>
+		<text class="result-tit-s">{{item.suggest}}</text>
+		<view class="result-txt">
+			<view class="txt-title text-black">测评结果概述：</view>
+			<view>
+				<text class="text-item" v-for="(item_a,index_a) in item.outline" :key="index_a">
+				{{index_a + 1}}. {{item_a}}</text>
+			</view>
+		</view>
+		<!-- AI推荐 -->
+		<block v-if="item.recommend.length>0">
+			<view class="txt-title text-black">AI为您推荐以下科室：</view>
+			<view class="keshi" v-for="(item_a,index_a) in item.recommend" :key="index_a">
+				<view class="keshi-item">
+					<text class="text-black">{{item_a.dep_name}}</text>
+					<text class="keshi-address">{{item_a.hospital}}</text>
+				</view>
+				<text class="guahao">去挂号</text>
+			</view>
+		</block>
+	</view>
+	<!-- 按钮 -->
+	<view style="height: 200rpx;"></view>
+	<view class="btn">
+		<button class="cancel" open-type="share">邀请朋友测一测</button>
+		<button class="submit" @click="again">再测一次</button>
+	</view>
+</template>
+
+<script setup lang="ts">
+	import {reactive, ref, watch} from 'vue'
+	import {onLoad,onShow,onShareAppMessage} from '@dcloudio/uni-app'
+	import {RequestApi} from '@/public/request'
+	import {Testres,ShareData} from '@/public/decl-type'
+	import {TEST} from '@/public/testing'
+	let type_id = ref('')
+	let choice_id = ref<string[]>([])
+	onLoad((event:any)=> {
+		const {type,topic_id} = JSON.parse(event.value)
+		type_id.value = type
+		choice_id.value = topic_id
+	})
+	let test_res = ref<Testres[]>([])
+	onShow(async()=> {
+		if(type_id.value == '001') {
+			var res:any = await RequestApi.Depression({value:choice_id.value})
+		} else if (type_id.value == '002') {
+			var res:any = await RequestApi.PreMature({value:choice_id.value})
+		} else {
+			var res:any = await RequestApi.InsoMnia({value:choice_id.value})
+		}
+		test_res.value = res.data.data
+		share_data.value = TEST.filter((item) => item.type == type_id.value)
+	})
+	const again = () => {
+		uni.navigateBack({
+			delta:1
+		})
+	}
+	// 定义分享给朋友时的标题和特定图片
+	var share_data = ref<ShareData[]>([])
+	onShareAppMessage(() => {
+		 return {
+		      title: share_data.value[0].share_title,
+		      path: share_data.value[0].share_path,
+			  imageUrl:share_data.value[0].share_url
+		    }
+	})
+</script>
+
+<style>
+	page{
+	      background: linear-gradient(to bottom,#d6e8ff 30%,#f2f7fb 55%, #f0f5f9 90%);
+		  padding: 20rpx;
+		  width: 100%;
+		  box-sizing: border-box;
+	  }
+	  .texts {
+		  line-height: 50rpx;
+		  margin-bottom: 40rpx;
+	  }
+	  .result-title {
+		  margin-bottom: 20rpx;
+	  }
+	  .text-black {
+		  font-weight: 600;
+	  }
+	  .result {
+		  padding: 20rpx;
+		  background-color: #fff;
+	  }
+	  .result-tit-b {
+		  display: block;
+		  color: #0066ff;
+		  margin-bottom: 20rpx;
+		  font-size: 40rpx;
+	  }
+	  .result-tit-s {
+		  display: block;
+		  color: #66ccff;
+		  margin-bottom: 20rpx;
+	  }
+	  .txt-title {
+		  margin-bottom: 30rpx;
+	  }
+	  .text-item {
+		  width: 100%;
+		  margin-bottom: 20rpx;
+		  display: inline-block;
+		  word-wrap: break-word;
+	  }
+	  .keshi {
+		  padding: 20rpx;
+		  background-color: #e4e6e7;
+		  display: flex;
+		  justify-content: space-between;
+		  align-items: center;
+		  margin-bottom: 20rpx;
+	  }
+	  .keshi-item {
+		  
+		  display: flex;
+		  flex-direction: column;
+		  /* align-items: center; */
+	  }
+	  .keshi-address {
+		  color: #afb9bf;
+	  }
+	  .guahao {
+		  color: #fff;
+		  padding: 10rpx;
+		  background-color: #0066ff;
+	  }
+	  .btn {
+	  	position: fixed;
+	  	bottom: 0;
+	  	left: 0;
+	  	background-color: #fff;
+		box-sizing: border-box;
+	  	width: 100%;
+	  	padding: 20rpx;
+	  	text-align: center;
+	  	box-shadow: #f7f7f7 0px 0px 20rpx 10rpx;
+		display: flex;
+		justify-content: space-between;
+	  }
+	  .btn button{
+		  font-size: 28rpx;
+		width: 280rpx;
+	  	height: 70rpx;
+	  	lighting-color: 70rpx;
+	  	margin: 0 20rpx;
+	  	/* padding: 20rpx; */
+	  }
+	  .btn .cancel {
+	  	background-color: #e5faff;
+	  	color: #3383ff;
+	  }
+	  .btn .submit {
+	  	background-color: #3383ff;
+	  	color: #fff;
+	  }
+</style>
---
+++ src/pages/self-test/topic.vue
@@ -0,0 +1,148 @@
+<template>
+	<view class="test-tit">
+		<text>测评进度</text>
+		<progress :percent="cur_percent"  class="progress" border-radius="30" activeColor="#0066ff" stroke-width="10"/>
+		<text>{{cur_index}}/{{data_length}}</text>
+	</view>
+	<!-- 题目 -->
+	<view class="test-problem structure">{{cur_data.topic}}</view>
+	<!-- 选项 -->
+	<view class="test-choices structure">
+		<view class="choices-type">
+			<text class="line"></text>
+			<text>单选题</text>
+		</view>
+		<view class="choices">
+			<view class="choices-item"  hover-class="hover-style" hover-stay-time="300"
+			 v-for="(item,index) in cur_data.options" :key="index"
+			 @click="changeNextPage(item.son_id)"
+			 v-if="cur_data.options.length>0"
+			 >{{item.title}}</view>
+		</view>
+	</view>
+	<Skeleton v-if="s_show"/>
+</template>
+
+<script setup lang="ts">
+	import {reactive, ref, watch} from 'vue'
+	import {onLoad,onShow} from '@dcloudio/uni-app'
+	import {RequestApi} from '@/public/request'
+	import {TestResult} from '@/public/decl-type'
+	import Skeleton from '@/skeleton/self-test-topic.vue'
+	//骨架屏的显示
+	const s_show = ref(true)
+	// 001 抑郁症 002 男性功能 003 失眠
+	let Type = ref('')
+	let Title = ref('')
+	onLoad((event:any) => {
+		const {type,name} = event
+		Type.value = type
+		Title.value = name
+		uni.setNavigationBarTitle({
+			title:Title.value
+		})
+	})
+	// 存储所有数据
+	let all_data = ref<TestResult[]>([])
+	// 存储当前页面的数据
+	let cur_data = reactive<TestResult>({
+		options: [],
+		topic: '',
+		_id: ''
+	})
+	// 存储所有数据的长度
+	let data_length = ref(0)
+	onShow(async()=> {
+		cur_index.value = 1
+		cur_index_s.value = 0
+		cur_percent.value = 0
+		question_id.value = []
+		if(Type.value=='001') {
+			var res:any = await RequestApi.DepressionTopics()
+		} else if(Type.value == '002') {
+			var res:any = await RequestApi.PrematureTopics()
+		} else {
+			var res:any = await RequestApi.InsomniaTopics()
+		}
+		all_data.value = res.data.data
+		data_length.value = res.data.data.length
+		cur_data = res.data.data[0]
+		cur_percent.value = (100 / data_length.value)
+		s_show.value = false
+	})
+	// 存储当前的题目的索引
+	let cur_index = ref(1)
+	// 存储当前加入题目选项的索引
+	let cur_index_s = ref(0)
+	// 存储进度条的百分比
+	let cur_percent = ref(0)
+	// 存储所有题目选项的ID
+	let question_id = reactive<{value:string[]}>({value:[]})
+	const changeNextPage = (son_id:string) => {
+		cur_index_s.value++ 
+		if(cur_index_s.value < data_length.value) {
+			cur_index.value++
+			cur_data = all_data.value[cur_index_s.value]
+			cur_percent.value = 100*(cur_index.value)/(data_length.value)
+		}
+		question_id.value.push(son_id)
+	}
+	watch([cur_index_s,data_length],(newVal,oldVal)=> {
+		let obj = JSON.stringify({type:Type.value,topic_id:question_id.value})
+		if(newVal[0] == newVal[1]) {
+			uni.navigateTo({
+				url:"/pages/self-test/result?value=" + obj
+			})
+		}
+	})
+	
+</script>
+
+<style>
+	  page{
+	        background: linear-gradient(to bottom,#d6e8ff 30%,#f2f7fb 55%, #f0f5f9 90%);
+	    }
+	  .test-tit {
+		  display: flex;
+		  justify-content: space-between;
+		  padding: 20rpx;
+	  }
+	  .progress {
+		  flex: 1;
+		  margin: 0 20rpx;
+	  }
+	  .structure {
+		  padding: 20rpx;
+		  margin: 20rpx;
+	  }
+	  .test-problem {
+		  display: block;
+		  background-color: #fff;
+		  color: #000;
+		  border-radius: 20rpx;
+	  }
+	  .test-choices {
+		  background-color: #e2eaf5;
+		  color: #000;
+	  }
+	  .choices-type {
+		  display: flex;
+		  align-items: center;
+	  }
+	  .line {
+		  display: inline-block;
+		  width: 6rpx;
+		  height: 26rpx;
+		  background-color: #0066ff;
+		  margin-right: 10rpx;
+	  }
+	  .choices-item {
+		  padding: 20rpx;
+		  background-color: #fff;
+		  margin: 20rpx 0 20rpx 0;
+	  }
+	  .hover-style {
+		  background-color: #ebf4ff;
+		  color: #298cff;
+	  }
+</style>

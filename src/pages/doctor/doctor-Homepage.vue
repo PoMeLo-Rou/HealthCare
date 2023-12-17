7613b4d92254c7a8de749ae6d5e1bd6cf0111bc3
Nuyoah <13056489+zhou-jinfang@user.noreply.gitee.com>
2023-12-17 05:44:45 +0000

    首次上传
    Signed-off-by: Nuyoah <13056489+zhou-jinfang@user.noreply.gitee.com>

---
+++ src/pages/doctor/doctor-Homepage.vue
@@ -0,0 +1,193 @@
+<template>
+	<block v-for="(item,index) in doctor_data" :key="index">
+		<view class="structure top">
+			<view class="doctor-info">
+				<image :src="item.avatar" mode="aspectFill"></image>
+				<view class="info">
+					<view class="name">
+						<text class="text-black">{{item.name}}</text>
+						<text>{{item.post}}</text>
+					</view>
+					<view class="hospital">{{item.hospital}}</view>
+				</view>
+			</view>
+			<text>{{item.good_at}}</text>
+		</view>
+		<!-- 预约挂号 -->
+		<view class="reserve structure">
+			<view class="reserve-tit">预约挂号</view>
+			<view class="reserve-scroll">
+				<view class="reserve-l">
+					<text class="border">日期</text>
+					<text class="border">上午</text>
+					<text class="border">下午</text>
+				</view>
+				<scroll-view scroll-x="true" class="scroll" enable-flex="true">
+					<view class="date-item" v-for="(item_a,index_a) in item.App_ment" :key="index_a">
+						<view class="date border">
+							<text>{{item_a.day}}</text>
+							<text>{{item_a.week}}</text>
+						</view>
+						<view class="order border" :class="item_a.time[0].nu_source<=0?'dis':'selected'"
+						@click="selectTime(item_a.week,item_a.time[0].the_time,item_a.time[0].when)">
+						{{item_a.time[0].nu_source<=0?'':'预约'}}</view>
+						<view class="order border" :class="item_a.time[1].nu_source<=0?'dis':'selected'">
+							{{item_a.time[1].nu_source<=0?'':'预约'}}
+						</view>
+					</view>
+				</scroll-view>
+			</view>
+		</view>
+	</block>
+	<!-- 重要提示 -->
+	<view class="warning">
+		<view class="important">重要提示</view>
+		<view class="tip">1.本次预约就诊当天不可以取消预约，如需取消，请在就诊前一天24：00之前操作，累计取消或爽约三次可能会被列入医院黑名单，请按需预约</view>
+		<view>2.预约挂号暂不支持医保支付，若本次挂号使用手机在线支付，就诊过程中的门诊检验检查，处方费用可能不支持医保支付</view>
+	</view>
+	<!-- 弹窗 -->
+	<popus ref="component"/>
+	<Skeleton v-if="s_show"/>
+</template>
+
+<script setup lang="ts">
+	import {ref} from 'vue'
+	import {onLoad} from '@dcloudio/uni-app'
+	import {RequestApi} from '@/public/request'
+	import {DoctorData} from '@/public/decl-type'
+	import popus,{Data} from './components/pop-ups.vue'
+	import Skeleton from '@/skeleton/doctor-homepage.vue'
+	// 骨架屏的显示
+	let s_show = ref(true)
+	// 存储挂号医生的id
+	let Id = ref('')
+	// 存储医生的数据
+	let doctor_data = ref<DoctorData[]>([])
+	onLoad(async(event:any) => {
+		const {id} = event
+		Id.value = id
+		const res:any = await RequestApi.DoctorHome({_id:id})
+		doctor_data.value = res.data.data
+		s_show.value = false
+		
+	})
+	//弹窗实例
+	let component = ref()
+	// component.value.trigger()
+	// 点击日期中的预约，弹窗出现
+	const selectTime = (week:string,the_time:string[],when:number) => {
+		const value:Data = {
+			week, 
+			the_time, 
+			when,
+			 _id: Id.value,
+			show: true,
+		}
+		component.value.trigger(value)
+	}
+</script>
+
+<style>
+	page{
+	      background: linear-gradient(to bottom,#d6e8ff 30%,#f2f7fb 55%, #f0f5f9 90%);
+	  }
+	.structure {
+		margin: 20rpx;
+		padding: 20rpx;
+		background-color: #fff;
+		box-shadow: #f7f7f7 0 0 20rpx 10rpx;
+	}
+	.top {
+		line-height: 60rpx;
+	}
+	.doctor-info {
+		display: flex;
+		align-items: center;
+	}
+	.doctor-info image {
+		width: 160rpx;
+		height: 160rpx;
+		margin-right: 10rpx;
+	}
+	.hospital {
+		text-align: left;
+	}
+	.text-black {
+		font-size: 38rpx;
+		font-weight: 600;
+		margin-right: 6rpx;
+	}
+	.reserve {
+		border-collapse: collapse;
+	}
+	.reserve-tit {
+		font-size: 38rpx;
+	}
+	.reserve-scroll {
+		display: flex;
+	}
+	.reserve-l {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.reserve-l text {
+		width: 100rpx;
+		height: 168rpx;
+		line-height: 168rpx;
+		text-align: center;
+	}
+	.scroll {
+		/* height: 516rpx; */
+		white-space: nowrap;
+		width: 556rpx;
+		flex: 1;
+		overflow: auto;
+		/* width: 100%; */
+	}
+	.date-item {
+		display: inline-block;
+		width: 180rpx;
+	}
+	.date-item:first-child {
+		transform: translateY(-172rpx);
+	}
+	.date {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 168rpx;
+		/* line-height: 168rpx; */
+	}
+	.date text:first-child {
+		margin-top: 42rpx;
+	}
+	.order {
+		width: 100%;
+		height: 168rpx;
+		line-height: 168rpx;
+		text-align: center;
+	}
+	.border {
+		border: 1px solid #f0f3f5;
+	}
+	.warning {
+		padding: 20rpx;
+		font-size: 26rpx;
+	}
+	.important {
+		margin: 10rpx 0;
+	}
+	.tip {
+		margin-bottom: 10rpx;
+	}
+	.selected {
+		background-color: #0075ff;
+		color: #fff;
+	}
+	.dis {
+	/* 禁止点击 */
+		pointer-events: none;
+	}
+</style>
---
+++ src/pages/doctor/index.vue
@@ -0,0 +1,167 @@
+<template>
+	<view class="top">
+		<view class="all" :class="curIndex==-1?'selected':''" @click="selectAll">全部</view>
+		<scroll-view class="scroll" scroll-x="true">
+			<view class="items" @click="selectDate(index,item.dep_id,item.date)" v-for="(item,index) in doctor_time" :key="index">
+				<view class="date-item">
+					<text>{{item.date}}</text>
+					<text :class="index==curIndex?'selected':''">{{item.week}}</text>
+					<text>{{item.nu_source==1?'可约':'无号'}}</text>
+				</view>
+			</view>
+		</scroll-view>
+	</view>
+	<!-- 高度 -->
+	<view style="height: 170rpx;"></view>
+	<!-- 医生预约 -->
+	<view class="doctor" v-for="(item,index) in doctor_list" :key="index" @click="jumpRoute(item._id)">
+		<view class="doctor-l">
+			<image :src="item.avatar" mode="aspectFill"></image>
+			<view class="desc">
+				<text class="text-black">{{item.name}}</text>
+				<text>{{item.post}}</text>
+				<text>{{item.good_at}}</text>
+			</view>
+		</view>
+		<view class="doctor-r">
+			<text class="reserve">预约</text>
+		</view>
+	</view>
+	<point :show="show" :title="title"/>
+	<Skeleton v-if="s_show"/>
+</template>
+
+<script setup lang="ts">
+	import { ref, onMounted} from 'vue'
+	import {RequestApi} from '@/public/request'
+	import {DoctorTime,DoctorList} from '@/public/decl-type'
+	import {onLoad} from '@dcloudio/uni-app'
+	import Point from '@/com-components/point.vue'
+	import Skeleton from '@/skeleton/s-doctor-index.vue'
+	// 骨架屏的显示与隐藏
+	const s_show = ref(true)
+	// 存储科室的id
+	const depId = ref<string>('')
+	// 日期的数据
+	const doctor_time = ref<DoctorTime[]>([])
+	// 医生的数据
+	const doctor_list = ref<DoctorList[]>([])
+	const show = ref(false)
+	const title = ref('没有医生数据')
+	onLoad(async(event:any)=> {
+		const {id} = event
+		depId.value = id
+		const res:any = await Promise.all([RequestApi.TimeSele({dep_id:id}),RequestApi.AlldList({dep_id:id})])
+		doctor_time.value = res[0].data.data
+		doctor_list.value = res[1].data.data
+		if(doctor_list.value.length < 1) {
+			show.value = true
+		}
+		s_show.value= false
+	})
+	
+	// 日期选中的索引值
+	let curIndex = ref(-1)
+	// 选择全部时的医生数据
+	const selectAll = async() => {
+		const res:any = await RequestApi.AlldList({dep_id:depId.value})
+		doctor_list.value = res.data.data
+	}
+	
+	//根据时间选择
+	const selectDate = async (index:number,dep_id:string,date:string) => {
+		curIndex.value = index
+		const res:any = await RequestApi.EverydList({dep_id,week:date})
+		doctor_list.value = res.data.data
+		
+	}
+	//跳转到医生详情
+	const jumpRoute = (_id:string) => {
+		uni.navigateTo({
+			url:"/pages/doctor/doctor-Homepage?id=" + _id
+		})
+	}
+	
+</script>
+
+<style>
+	.top {
+		border-top: 1px solid #d3d3d8;
+		position: fixed;
+		left: 0;
+		top: 0;
+		right: 0;
+		width: 100%;
+		display: flex;
+		box-shadow: #f7f7f7 0 0 20rpx 10rpx;
+	}
+	.all {
+		width: 150rpx;
+		height: 150rpx;
+		line-height: 150rpx;
+		text-align: center;
+		
+		border-right: 1px solid #d3d3d8;
+	}
+	.scroll {
+		width: 100%;
+		flex: 1;
+		height: 150rpx;
+		line-height:50rpx;
+		white-space: nowrap;
+		overflow: scroll;
+	}
+	.date-item {
+		width: 150rpx;
+		height: 150rpx;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	.items {
+		display: inline-block;
+		font-size: 30rpx;
+	}
+	.doctor {
+		display: flex;
+		padding: 40rpx 20rpx;
+		justify-content: space-between;
+		/* align-items: center; */
+		box-shadow: #f7f7f7 0 0 20rpx 10rpx;
+	}
+	.doctor-l {
+		display: flex;
+		align-items: center;
+	}
+	.doctor-l image {
+		width: 160rpx;
+		height: 160rpx;
+		border-radius: 50%;
+		margin-right: 8rpx;
+	}
+	.desc {
+		display: flex;
+		flex-direction: column;
+		/* align-items: center; */
+	}
+	.doctor-r {
+		margin-top: 80rpx;
+		width: 190rpx;
+		height: 80rpx;
+	}
+	.reserve {
+		margin-top: 20rpx;
+		width: 100%;
+		padding: 10rpx 30rpx;
+		border: 1px solid #0099ff;
+		border-radius: 40rpx;
+	}
+	.text-black {
+		font-size: 38rpx;
+		font-weight: 600;
+	}
+	.selected {
+		color: #0099ff;
+	}
+</style>

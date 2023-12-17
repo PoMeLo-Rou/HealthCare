bb104e8623ee2cb8f0c75c69d92f4b228d444c59
Nuyoah <13056489+zhou-jinfang@user.noreply.gitee.com>
2023-12-17 05:55:17 +0000

    首次上传
    Signed-off-by: Nuyoah <13056489+zhou-jinfang@user.noreply.gitee.com>

---
+++ src/pages/phy-exam/Details.vue
@@ -0,0 +1,242 @@
+<template>
+	<!-- 顶部图片区域 -->
+	<view class="detail-top" v-for="(item,index) in phy_detail" :key="index">
+		<image :src="item.image" mode="aspectFill"></image>
+		<view class="top-num">
+			<text class="top-price">￥{{item.price}}</text>
+			<text>已约{{item.sales}}</text>
+		</view>
+		<text class="top-tit">{{item.title}}</text>
+		<view style="height: 20rpx;"></view>
+	</view>
+	<!-- 体检时间选择 -->
+	<view class="reserve-time">
+		<text class="reserve-tit text-black">体检时间选择</text>
+		<scroll-view scroll-x="true" class="scroll-view_H">
+			<view @click="activeIndex=index,s_data.phy_time = item.date" v-if="phy_detail.length > 0" :class="index==activeIndex?'selected':''" class="scroll" v-for="(item,index) in phy_detail[0].date" :key="index">
+				<view class="scroll-item">
+					<text>{{item.date}}</text>
+					<text>{{item.week}}</text>
+				</view>
+			</view>
+		</scroll-view>
+	</view>
+	<!-- 适用人群 -->
+	<view class="structure">
+		<text class="fit-tit text-black">适用人群</text>
+		<view class="fit-items">
+			<block v-for="(item,index) in phy_detail" :key="index">
+				<view class="fit-item" v-for="(item_a,index_a) in item.crowd" :key="index_a">
+					<image :src="item_a.image" mode="aspectFill"></image>
+					<text>{{item_a.name}}</text>
+				</view>
+			</block>
+		</view>
+	</view>
+	<!-- 套餐项目 -->
+	<view class="structure">
+		<text class="structure-tit text-black">套餐项目</text>
+		<view class="projects">
+			<view v-if="phy_detail.length>0" class="project-item" v-for="(item,index) in phy_detail[0].project" :key="index">
+				<text class="project-tit">{{item.title}}</text>
+				<view class="project-table" v-for="(item_a,index_a) in item.content" :key="index_a">
+					<text class="table-l">{{item_a.thing}}</text>
+					<text class="table-r">{{item_a.details}}</text>
+				</view>
+			</view>
+		</view>
+	</view>
+	<Semember ref="member"/>
+	<view style="height: 300rpx;"></view>
+	<!-- 按钮 -->
+	<view class="btn">
+		<text v-if="phy_detail.length>0" class="check-price">检测费用: ￥{{phy_detail[0].price}}</text>
+		<text class="submit" @click="chooseMember">选择成员</text>
+	</view>
+</template>
+
+<script setup lang="ts">
+	
+	import {onLoad} from '@dcloudio/uni-app'
+	import {reactive, ref} from 'vue'
+	import {RequestApi} from '@/public/request'
+	import {PhyDetailData} from '@/public/decl-type'
+	import Semember,{SData} from '@/com-components/se-member.vue'
+	const phy_detail = ref<PhyDetailData[]>([])
+	const activeIndex = ref(-1)
+	const member = ref()
+	const s_data = reactive<SData>({
+		phy_name:'',
+		phy_time:'',
+		show:true
+	})
+	onLoad(async(event:any)=> {
+		s_data.phy_name = event.title
+		const res:any = await RequestApi.PhyDateil({id:event.id})
+		phy_detail.value = res.data.data
+	})
+	const chooseMember = () => {
+		member.value.trigger(s_data)
+	}
+	
+</script>
+
+<style scoped>
+	.detail-top {
+		width: 100%;
+		box-shadow: #f7f7f7 0 0 20rpx 10rpx;
+	}
+	.detail-top image {
+		width: 100%;
+		height: 440rpx;
+	}
+	.top-num {
+		padding: 20rpx;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		font-size: 34rpx;
+	}
+	.top-price {
+		color: #ff9933;
+	}
+	.top-tit {
+		padding: 20rpx;
+		font-size: 38rpx;
+		font-weight: 600;
+	}
+	.reserve-time {
+		margin: 20rpx;
+		padding: 20rpx;
+		box-shadow: #f7f7f7 0px 0px 20rpx 10rpx;
+		height: 150rpx;
+	}
+	.reserve-tit {
+		margin-bottom: 20rpx;
+	}
+	.text-black {
+		font-size: 30rpx;
+		font-weight: 700;
+	}
+	scroll-view {
+		margin-top: 20rpx;
+	}
+	.scroll-item {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.scroll-view_H {
+		white-space: nowrap;
+	}
+	.scroll {
+		display: inline-block;
+		width: 100rpx;
+		height: 75rpx;
+		padding: 12rpx;
+		font-size: 30rpx;
+		border-radius: 10rpx;
+	}
+	.selected {
+		background-color: #3383ff;
+		color: #ffffff;
+	}
+	.structure {
+		padding: 20rpx;
+		margin: 20rpx;
+		box-shadow: #f7f7f7 0 0 20rpx 10rpx;
+	}
+	.fit-tit {
+		height: 38rpx;
+		line-height: 38rpx;
+	}
+	.fit-items {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	.fit-item {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		text-align: center;
+		padding: 20rpx 0 20rpx 0;
+	}
+	.fit-item image {
+		width: 140rpx;
+		height: 140rpx;
+		margin-bottom: 20rpx;
+	}
+	.structure-tit {
+		height: 38rpx;
+		line-height: 38rpx;
+	}
+	.project-item {
+		margin: 20rpx 0 20rpx 0;
+		font-size: 38rpx;
+	}
+	.project-tit {
+		display: block;
+		height: 70rpx;
+		line-height: 70rpx;
+		text-align: center;
+		background-color: #d1e0e0;
+		margin-bottom: 20rpx;
+	}
+	.project-table {
+		display: flex;
+		justify-content: space-between;
+		margin-bottom: 10rpx;
+	}
+	.table-l {
+		width: 260rpx;
+		background-color: #d1e0e0;
+		text-align: center;
+		padding: 20rpx;
+		padding-top: 40rpx;
+		box-sizing: border-box;
+		margin-right: 6rpx;
+	}
+	.table-r {
+		display: inline-block;
+		flex: 1;
+		width: 176rpx;
+		background-color: #d1e0e0;
+		padding: 4rpx;
+		word-wrap: break-word;
+		height: auto;
+	}
+	.btn {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		background-color: #fff;
+		width: 100%;
+		padding: 30rpx;
+		text-align: center;
+		box-shadow: #f7f7f7 0px 0px 20rpx 10rpx;
+		/* width: 208rpx; */
+		height:150rpx;
+		box-sizing: border-box;
+	}
+	.check-price {
+		float: left;
+		color: #ff9900;
+		font-size: 30rpx;
+		height: 80rpx;
+		line-height: 80rpx;
+	}
+	.btn .submit {
+		box-sizing: border-box;
+		float: right;
+		background-color: #3383ff;
+		color: #fff;
+		margin: 0 30rpx;
+		padding: 20rpx 50rpx;
+		border-radius: 10rpx;
+	}
+	.selected {
+		background-color: #3383ff;
+		color: #ffffff;
+	}
+</style>
---
+++ src/pages/phy-exam/index.vue
@@ -0,0 +1,180 @@
+<template>
+	<view class="tijian-nav">
+		<view class="nav-item" v-for="(item,index) in phy_term" :key="index" @click="shaixuan(index,item.filter_val)">
+			<text>{{item.query_val}}</text>
+			<image :src="index==0?'/static/other/shaixuan-jiantou.png':'/static/other/shaixuan.png'"></image>
+		</view>
+	</view>
+	<!-- 下拉列表 -->
+	<view class="down-list" v-if="down_show">
+		<view class="down-item">
+			<text v-if="phy_term.length>0" v-for="(item,index) in phy_term[0].filter_val" :key="index" @click="changeSelect(item)">{{item}}</text>
+		</view>
+		<view class="mask"></view>
+	</view>
+	<!-- 体检套餐 -->
+	<view style="height: 140rpx;"></view>
+	<view class="tijian-list" @click="goDetails(item._id,item.title)" v-for="(item,index) in phy_data" :key="index">
+		<text class="list-tit text-black">{{item.title}}</text>
+		<view class="list-img">
+			<image :src="item.image" mode="aspectFill"></image>
+			<view class="desc">
+				<text class="text-black">{{item.be_suit}}</text>
+				<view class="descs">{{item.describe}}</view>
+			</view>
+		</view>
+		<view class="list-price">
+			<text>已约 {{item.sales}}</text>
+			<text>￥{{item.price}}</text>
+		</view>
+	</view>
+	<Skeleton v-if="s_show"/>
+</template>
+
+<script setup lang="ts">
+	import {onMounted,reactive,ref} from 'vue'
+	import {RequestApi} from '@/public/request'
+	import {PhyTerm,PhyData} from '@/public/decl-type'
+	import Skeleton from '@/skeleton/phy-exam-index.vue'
+	// 骨架屏的显示
+	let s_show = ref(true)
+	let phy_term = ref<PhyTerm[]>([])
+	let phy_data = ref<PhyData[]>([])
+	onMounted(async ()=> {
+		const res:any = await Promise.all([RequestApi.PhyTerm(),RequestApi.PhysGet()])
+		phy_term.value = res[0].data.data
+		phy_data.value = res[1].data.data
+		s_show.value = false
+	})
+	const down_show = ref(false)
+	const filterData = reactive({type:'',sales:'',price:''})
+	const shaixuan = (index:number,value :string[]) => {
+		if(index==0) {
+			down_show.value = down_show.value?false:true
+		}else if(index ==1) {
+			// 按销量
+			filterData.sales = value[0]
+			phy_term.value[index].filter_val[0] =value[0] == 'desc'?'asc':'desc'
+			QueryData()
+		} else {
+			// 按价格
+			filterData.price = value[0]
+			phy_term.value[index].filter_val[0] =value[0] == 'desc'?'asc':'desc'
+			QueryData()
+		}
+	}
+	// 查询筛选后的数据
+	const QueryData = async () => {
+		const res:any = await RequestApi.PhyQuery(filterData)
+		phy_data.value = res.data.data
+		
+	}
+	const changeSelect = (value: string) => {
+		phy_term.value[0].query_val = value
+		down_show.value = false
+		filterData.type = value
+		QueryData()
+	}
+	// 点击套餐列表，跳转到套餐详情页
+	const goDetails = (id:string,title:string) => {
+		uni.navigateTo({
+			url:'/pages/phy-exam/Details?id='+id+'&title='+title
+		})
+	}
+</script>
+
+<style scoped>
+	.tijian-nav {
+		 position: fixed;
+		top: 0;
+		right: 0;
+		left: 0;
+		padding: 40rpx 50rpx;
+		box-shadow: #f7f7f7 0 0 20rpx 10rpx;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		background-color: #fff;
+	}
+	.nav-item {
+		font-size: 28rpx;
+	}
+	.nav-item image {
+		width: 20rpx;
+		height: 20rpx;
+		margin-left: 10rpx;
+	}
+	.tijian-list {
+		margin: 20rpx;
+		padding: 20rpx;
+		box-shadow: #f7f7f7 0 0 20rpx 10rpx;
+	}
+	.list-tit {
+		height: 80rpx;
+		line-height: 80rpx;
+		font-size: 34rpx;
+	}
+	.text-black {
+		font-weight: 600;
+		color: black;
+	}
+	.list-img {
+		display: flex;
+		align-items: center;
+	}
+	.list-img image {
+		width: 200rpx;
+		height: 180rpx;
+	}
+	.desc {
+		width: 800rpx;
+		height: 200rpx;
+		display: flex;
+		flex-direction: column;
+		font-size: 30rpx;
+		color:#cccccc;
+		margin-left: 20rpx;
+	}
+	.desc text {
+		margin: 10rpx 0;
+	}
+	.descs {
+		 display: inline-block;
+		  width: 490rpx;
+		  word-wrap: break-word;
+	}
+	.list-price {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		color: #ff9900;
+		font-size: 32rpx;
+	}
+	.list-price text {
+		margin: 40rpx 0 10rpx 0;
+	}
+	.down-item {
+		 background-color: #ffffff;
+		    padding: 0 50rpx;
+		    position: fixed;
+		    top: 98rpx;
+		    left: 0;
+		    right: 0;
+			z-index: 10;
+	}
+	.down-item text {
+		display: block;
+		width: 100%;
+		height: 40rpx;
+		line-height: 40rpx;
+		margin: 20rpx 0;
+	}
+	.mask {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		top: 100rpx;
+		background-color: rgba(0,0,0,0.5);
+	}
+</style>
